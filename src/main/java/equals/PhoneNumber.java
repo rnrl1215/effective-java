6c159c22742bb17dd2c31afd8415fef0ff01c062
skahn <d1215@naver.com>
2022-05-09 22:55:50 +0900

    feat: commit

---
+++ src/main/java/equals/PhoneNumber.java
@@ -0,0 +1,33 @@
+package equals;
+
+import org.apache.catalina.realm.JAASRealm;
+
+public class PhoneNumber {
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(short areaCode, short prefix, short lineNum) {
+        this.areaCode = areaCode;
+        this.prefix = prefix;
+        this.lineNum = lineNum;
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max)
+            throw new IllegalArgumentException(arg + ":" + val);
+        return (short) val;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (!(obj instanceof PhoneNumber))
+            return false;
+
+        PhoneNumber pn = (PhoneNumber)obj;
+
+        return pn.lineNum == lineNum
+                && pn.prefix == prefix
+                && pn.areaCode == areaCode;
+    }
+}
